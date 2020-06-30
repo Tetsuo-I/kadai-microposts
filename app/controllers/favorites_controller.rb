f70a6c25498dd6fca98b0bd95919327ff63b13ae
Tetsuo.Ito <tetsuo.ito0424@gmail.com>
2020-06-30 10:43:51 +0000

    favorites

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,16 @@
+class FavoritesController < ApplicationController
+  before_action :require_user_logged_in
+  def create
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.favorite(micropost)
+    flash[:success] = 'いいねしました！'
+    redirect_to root_path
+  end
+
+  def destroy
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unfavorite(micropost)
+    flash[:success] = 'いいねを解除しました。'
+    redirect_to root_path
+  end
+end
